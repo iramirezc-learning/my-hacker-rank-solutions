85a43d80a33dacb8a623fa75925b7dbe3dece955
Isaac Ramirez <iramirezc@live.com.mx>
2021-02-08 00:01:44 -0600

    Add jest helpers file

---
+++ helpers.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+global.eachFile = (dirname, cb) => {
+  const inputPath = path.join(dirname, "input");
+  const outputPath = path.join(dirname, "output");
+  const fileNames = fs.readdirSync(inputPath);
+
+  fileNames.forEach((fileName) => {
+    const input = fs
+      .readFileSync(path.join(inputPath, fileName))
+      .toString()
+      .trim();
+
+    const output = fs
+      .readFileSync(path.join(outputPath, fileName))
+      .toString()
+      .trim();
+
+    cb(fileName, input, output);
+  });
+};
