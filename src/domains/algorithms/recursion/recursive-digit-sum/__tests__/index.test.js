a6b1e8f75bae35c585b5a2939d9167edd6abdb74
Isaac Ramirez <iramirezc@live.com.mx>
2021-02-08 02:29:57 -0600

    Solved: Recursive Digit Sum

---
+++ src/domains/algorithms/recursion/recursive-digit-sum/__tests__/index.test.js
@@ -0,0 +1,9 @@
+const solve = require("../index");
+
+describe("Recursive Digit Sum", () => {
+  eachFile(__dirname, (fileName, input, output) => {
+    test(`Test Case: ${fileName}`, () => {
+      expect(solve(input)).toEqual(output);
+    });
+  });
+});
