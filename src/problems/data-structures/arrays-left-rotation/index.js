2495c6134fd26a52fa161e0b0534235c8e0cf0de
Isaac Ramirez <iramirezc@live.com.mx>
2022-05-28 02:30:13 -0500

    Renaming

--- src/problems/data-structures/arrays-left-rotation/index.js
+++ src/problems/data-structures/arrays-left-rotation/index.js
@@ -1,10 +1,10 @@
-const challenge = require("./arrays-left-rotation");
+const leftRotation = require("./arrays-left-rotation");
 
 function main(inputAsString) {
   const input = inputAsString.replace(/\s+$/g, "").split("\n");
   const [, d] = input[0].split(" ").map((n) => parseInt(n, 10));
   const a = input[1].split(" ").map((n) => parseInt(n, 10));
-  const result = challenge(a, d);
+  const result = leftRotation(a, d);
 
   return result.join(" ");
 }
